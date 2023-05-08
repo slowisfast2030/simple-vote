c96ef43ebfe7ac47370552f62ad56cc46b538f89
slowisfast <slowisfast2030@163.com>
2023-05-08 11:49:29 +0800

    less is more

--- learn.js
+++ learn.js
@@ -24,3 +24,37 @@ const numbers2 = [1, 2, 3, 4, 5];
 numbers2.forEach(function (num) {
   console.log(num); // 依次打印1, 2, 3, 4, 5
 });
+
+/**
+accounts.forEach(function(account){balance = await web3.eth.getBalance(account)})
+SyntaxError: await is only valid in async functions and the top level bodies of modules
+
+这个错误的原因是你在一个普通的函数中使用了await关键字，
+而await只能在异步函数（async function）或者模块的顶层中使用。
+await关键字用于等待一个Promise对象的结果，而web3.eth.getBalance是一个返回Promise对象的异步函数。
+ */
+
+// 要解决这个错误，你有两种方法：
+// 一种方法是把forEach的回调函数改成异步函数，即在function前面加上async关键字，例如：
+accounts.forEach(async function(account) {
+    balance = await web3.eth.getBalance(account);
+    // do something with balance
+  });
+
+// 另一种方法是不使用await关键字，而是使用then方法来处理Promise对象的结果，例如：
+accounts.forEach(function(account) {
+    web3.eth.getBalance(account).then(function(balance) {
+      // do something with balance
+    });
+  });
+
+// 改为回调函数
+accounts.forEach(function(account) {
+    web3.eth.getBalance(account, function(err, balance) {
+      if (err) {
+        console.error(err); // 处理错误
+      } else {
+        // do something with balance
+      }
+    });
+  });

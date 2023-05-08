d98a598857d104368f015c984d402699f89078c3
slowisfast <slowisfast2030@163.com>
2023-05-08 10:58:24 +0800

    less is more

--- migrations/1_deploy_voting.js
+++ migrations/1_deploy_voting.js
@@ -1,5 +1,9 @@
 var Voting = artifacts.require("Voting.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(Voting, ['Rama', 'Nick', 'Jose']);
+    var bytesArray = ["0x1234", "0x5678", "0x9abc"];
+    deployer.deploy(Voting, bytesArray);
     }
+
+// 这里确实是一个好问题。
+// 构造函数初始化是一个bytes32数组。那么这里如何进行初始化呢？
